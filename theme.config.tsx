b3c08d1c280cd17e0cdb4e8be38c48015d950092
심민성 <bighammer1753@gmail.com>
2024-06-25 03:59:56 +0900

    feat: 설정

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+export default {
+    footer: <p>MIT 2023 © Nextra.</p>,
+    head: ({title, meta}) => (
+        <>
+            {meta.description && (
+                <meta name="description" content={meta.description}/>
+            )}
+            {meta.tag && <meta name="keywords" content={meta.tag}/>}
+            {meta.author && <meta name="author" content={meta.author}/>}
+        </>
+    ),
+    readMore: 'Read More →',
+    postFooter: null,
+    darkMode: false,
+}
