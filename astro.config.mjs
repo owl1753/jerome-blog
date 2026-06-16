c7992aaf97dc92c5ddd6be52d24c5e7adc01af28
owl1753 <bighammer1753@gmail.com>
2026-06-16 11:15:42 +0900

    feat: 폰트 및 헤더 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,60 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    fonts: [{
+        provider: fontProviders.local(),
+        name: "Pretendard",
+        cssVariable: "--font-pretendard",
+        options: {
+            variants: [
+                {
+                    src: ["./src/assets/fonts/Pretendard-Black.woff", "./src/assets/fonts/Pretendard-Black.woff2"],
+                    weight: 900,
+                    style: "normal"
+                },
+                {
+                    src: ["./src/assets/fonts/Pretendard-Black.woff", "./src/assets/fonts/Pretendard-ExtraBold.woff2"],
+                    weight: 800,
+                    style: "normal"
+                },
+                {
+                    src: ["./src/assets/fonts/Pretendard-Bold.woff", "./src/assets/fonts/Pretendard-Bold.woff2"],
+                    weight: 700,
+                    style: "normal"
+                },
+                {
+                    src: ["./src/assets/fonts/Pretendard-Semibold.woff", "./src/assets/fonts/Pretendard-Semibold.woff2"],
+                    weight: 600,
+                    style: "normal"
+                },
+                {
+                    src: ["./src/assets/fonts/Pretendard-Medium.woff", "./src/assets/fonts/Pretendard-Medium.woff2"],
+                    weight: 500,
+                    style: "normal"
+                },
+                {
+                    src: ["./src/assets/fonts/Pretendard-Regular.woff", "./src/assets/fonts/Pretendard-Regular.woff2"],
+                    weight: 400,
+                    style: "normal"
+                },
+                {
+                    src: ["./src/assets/fonts/Pretendard-Light.woff", "./src/assets/fonts/Pretendard-Light.woff2"],
+                    weight: 300,
+                    style: "normal"
+                },
+                {
+                    src: ["./src/assets/fonts/Pretendard-ExtraLight.woff", "./src/assets/fonts/Pretendard-ExtraLight.woff2"],
+                    weight: 200,
+                    style: "normal"
+                },
+                {
+                    src: ["./src/assets/fonts/Pretendard-Thin.woff", "./src/assets/fonts/Pretendard-Thin.woff2"],
+                    weight: 100,
+                    style: "normal"
+                },
+            ]
+        }
+    }]
+});
